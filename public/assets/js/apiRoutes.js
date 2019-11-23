1a33fabaa00e054fe12f9097be3428d018f9df62
Levi Giraud <giraudlevi@gmail.com>
2019-11-22 20:19:33 -0700

    added logic to test the basic get for api data, it worked

---
+++ public/assets/js/apiRoutes.js
@@ -0,0 +1,14 @@
+var notesData = require("./../../../db/db");
+
+module.exports = function(app) {
+
+  app.get("/api/notes", function(req, res) {
+    res.json(notesData);
+  });
+
+  app.post("/api/notes", function(req, res) {
+      notesData.push(req.body);
+      res.json(true);  
+  });
+
+};
